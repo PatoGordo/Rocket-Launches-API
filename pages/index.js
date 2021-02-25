a8605de91cf96520bb57093df25c789ef0e445ef
PatoGordo <icariusv@gmail.com>
2021-02-25 17:43:18 +0000

    remove .next directory and news

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+		<div>
+			<h2>Welcome to Rocket Launches API</h2>
+			<h3>End points</h3>
+			<a href="/api/launches">/api/launches [GET]</a><br />
+			<a href="/api/news">/api/news [GET]</a>
+			<br />
+			<br />
+			<br />
+			<a href="https://github.com/PatoGordo/Rocket-Launches-API">Github Project</a>
+			<br />
+			<a href="https://github.com/PatoGordo/Rocket-Launches">Website</a>
+		</div>
+	)
 }
 
 export default HomePage
